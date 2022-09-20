e201ddae109949c21dbfbda4c8ce02215c1daf80
1004jaein <105484114+1004jaein@users.noreply.github.com>
2022-09-20 19:48:58 +0900

    Create 04_example_2.ino

---
+++ 04_example_2.ino
@@ -0,0 +1,12 @@
+void setup() {
+  //Initiallize serial and wait for port to open;
+  Serial.begin(115200);
+  while(!Serial){
+    ; // wait for serial port to connect. Needed for native USB
+  }
+}
+
+void loop() {
+  Serial.println("Hello World!");
+  delay(1000);
+}
